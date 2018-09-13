f2d3fd93bf251e7322205b4185b8bce2eb3dd95a
Andrea Telatin <andrea@telatin.com>
2018-09-13 10:35:06 +0100

    Speeding up

--- tag_search/tradis_csv_to_heatmap.pl
+++ tag_search/tradis_csv_to_heatmap.pl
@@ -7,11 +7,13 @@ use Text::CSV;
 use File::Basename;
 my $opt_version = undef;
 my $opt_debug   = undef;
-my $opt_min_q   = 0.05;
+my $opt_min_q   = 0.01;
+my $opt_key     = 'cpm';
 my $opt_min_check = 1;
 my $opt_grep;
 my $opt_strip;
 my $opt_nogrep;
+my $opt_keep_ext;
 my @FILES;
 my %COUNTERS;
 my $csv_parser = Text::CSV->new(
@@ -23,28 +25,39 @@ my $csv_parser = Text::CSV->new(
 );
 
 my $GetOptions = GetOptions(
-  'v|version'    => \$opt_version,
-  'd|debug'      => \$opt_debug,
-  'g|grep=s'     => \$opt_grep,     # Skip files not containing this string
-  'n|no-grep=s'  => \$opt_nogrep,   # Skip files  containing this string
-  's|strip=s'    => \$opt_strip,    # Strip this string from file names
-  'q|minq=f'     => \$opt_min_q,    # minimum Q value in at least 1 sample
-  'z|minsampl=i' => \$opt_min_check,
+  'version'          => \$opt_version,
+  'debug'            => \$opt_debug,
+
+  'q|min-q=f'        => \$opt_min_q,    # minimum Q value in at least 1 sample
+  'z|min-sample=i'  => \$opt_min_check,
+  'k|key=s'          => \$opt_key,
+
+  'g|grep=s'         => \$opt_grep,     # Skip files not containing this string
+  'v|anti-grep=s'    => \$opt_nogrep,   # Skip files  containing this string
+  's|strip=s'        => \$opt_strip,    # Strip this string from file names
+  'k|keep-ext'       => \$opt_keep_ext,
 );
+
+die " FATAL ERROR: -k,--key error: valid values are 'cpm', 'logfc' or 'q'\n"
+  if ($opt_key !~/^(cpm|logfc|q)$/);
 
 my %matrix = ();
 foreach my $input_file (@ARGV) {
   $COUNTERS{'files_total'}++;
-  open(my $data, '<', $input_file) or die "Could not open '$input_file' $!\n";
   deb(" - $input_file");
+
+  open(my $data, '<', $input_file)
+    or die "Could not open '$input_file' $!\n";
+
   if ( (defined $opt_grep and $input_file!~/$opt_grep/) or (defined $opt_nogrep and $input_file=~/$opt_nogrep/ ) ) {
-    deb("\tskipping");
+    deb("\t - skipping");
     next;
   }
   $COUNTERS{'files_added'}++;
   my $input = basename($input_file);
   $input=~s/$opt_strip// if (defined $opt_strip);
-  deb("\t$input");
+  $input=~s/^([^\.]+)/\1/ if (! defined $opt_keep_ext);
+  deb("\t - $input");
   push(@FILES, $input);
   my $c = 0;
   while (my $row = $csv_parser->getline ($data)) {
@@ -56,7 +69,9 @@ foreach my $input_file (@ARGV) {
     my $p_value   = $row->[5];
     my $q_value   = $row->[6];
     $matrix{$gene_name}{$input}{'cpm'} = $log_cpm;
+    $matrix{$gene_name}{$input}{'logfc'} = $log_fc;
     $matrix{$gene_name}{$input}{'q'}   = $q_value;
+    $COUNTERS{$input}+=$log_cpm;
   }
 }
 
@@ -76,7 +91,7 @@ foreach my $gene (sort keys %matrix) {
   foreach my $file (sort @FILES) {
     my $sep = "\t";
     $sep = "\n" if ($file eq $FILES[$#FILES]);
-    my $val = 0 + $matrix{$gene}{$file}{'cpm'};
+    my $val = 0 + $matrix{$gene}{$file}{$opt_key};
     $check++ if ($matrix{$gene}{$file}{'q'} < $opt_min_q);
     $line .= "$val$sep";
   }
@@ -95,4 +110,23 @@ foreach my $key (sort keys %COUNTERS) {
 sub deb {
   return if ! $opt_debug;
   say STDERR $_[0];
+}
+
+sub help {
+  print STDERR <<END;
+
+  Usage:
+  tradis_csv_to_heatmap.pl [options] CSV_FILE1 CSV_FILE2 ...
+
+  Options:
+    -q, --min-q
+                  Q-value must be lower than this in at least
+                  'z' samples
+    -z, --min-sample
+                  Minimum number of samples that must have q-value
+                  lower than -q
+    -k, --key
+                  Create a matrix based on key.
+                  Values are: 'cpm' (default), 'logfc', 'q'
+END
 }
