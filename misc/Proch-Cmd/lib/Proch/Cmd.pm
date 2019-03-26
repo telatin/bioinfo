c6b2b1a7cdc991e7db89c6f67d313614985037cf
Ubuntu <ubuntu@dash.novalocal>
2019-03-26 10:44:44 +0000

    Dist::Zilla conf

--- misc/Proch-Cmd/lib/Proch/Cmd.pm
+++ misc/Proch-Cmd/lib/Proch/Cmd.pm
@@ -1,5 +1,26 @@
 use strict;
 use warnings;
 package Proch::Cmd;
+$Proch::Cmd::VERSION = 0.001;
+# ABSTRACT: Execute shell commands controlling inputs and outputs
+
+use 5.014;
+use Moose; 
+use Data::Dumper;
+use Carp qw(confess);
+
+has debug => ( is => 'rw', isa => 'Bool');
+
+has command => (
+    is => 'ro',  
+    required => 1, 
+    isa => 'Str',
+);
+
+has logfile => (
+	is => 'ro',
+	isa => 'Str',
+);
+
 
 1;
