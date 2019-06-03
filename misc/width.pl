e1feeb5223f80cdb52a6b6ce7d29a2b45deb56a6
Ubuntu <ubuntu@robin.novalocal>
2019-06-03 15:42:33 +0000

    updatge demo

--- misc/width.pl
+++ misc/width.pl
@@ -4,8 +4,17 @@ use Term::Size::ReadKey;
 my ($columns, $rows) = Term::Size::ReadKey::chars *STDOUT{IO};
 my ($x, $y) = Term::Size::ReadKey::pixels;
 
+my $title = "Screen Frame v. 1.0";
 say "+", "-" x ($columns - 2), "+";
-for (my $i = 1; $i <= ($rows - 3); $i++) {
+say "|",
+	" " x ( ($columns - 2 - length($title)) / 2), 
+	$title,
+	" " x ( ($columns - 2 - length($title)) / 2), 
+	"|";
+say "+", "-" x ($columns - 2), "+";
+
+for (my $i = 1; $i <= ($rows - 5); $i++) {
  say "|", " " x ($columns - 2), "|";
 }
 say "+", "-" x ($columns - 2), "+";
+
